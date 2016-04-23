7393343c9ff9b240f2bf0356cc355c93407f4b43
hung3102 <daovanhung3107@yahoo.com>
2016-04-23 11:49:30 +0700

    commit to do webservice

--- backend/views/school-report/viewPdf.php
+++ backend/views/school-report/viewPdf.php
@@ -1,4 +1,5 @@
 <?php
+use common\models\Subject;
 ?>
 <div id="SR">
 	<div id="cover">
@@ -123,4 +124,226 @@
 			</table>
 		</div>
 	</div>
+<!-- year evaluation here -->
+	<?php if($model->yearEvaluations != null) {
+		for($i = 0; $i < sizeof($model->yearEvaluations); $i++) { ?>
+	<pagebreak />
+	<div id="year_evaluation">
+		<div id="part1">
+			<div id="info">
+				<table>
+					<tr>
+						<td colspan="5"><?= '<span style="font-weight:bold">Họ và tên: </span>'.$model->student->name ?></td>
+						<td class="class" colspan="3"><?= '<span style="font-weight:bold">Lớp </span>'.$model->studyProcesses[$i]->class ?></td>
+						<td class="year" colspan="5"><?= '<span style="font-weight:bold">năm học </span>'.$model->studyProcesses[$i]->fromYear.' - '.$model->studyProcesses[$i]->toYear ?></td>
+					</tr>
+					<tr>
+						<td colspan="3"><?= '<span style="font-weight:bold">Ban:  </span>'.$model->yearEvaluations[$i]->studyDepartment ?></td>
+						<td class="advantage_subjects" colspan="10">
+							<?= '<span style="font-weight:bold">Các môn học nâng cao: </span>'.$model->yearEvaluations[$i]->getAdvantageSubjects() ?>
+						</td>
+					</tr>
+				</table>
+			</div>
+			<table id="year_table">
+				<thead>
+					<tr>
+						<th class="subject_name" rowspan="2">Môn học/Hoạt động GD</th>
+						<th class="score" colspan="3">Điểm trung bình hoặc xếp loại các môn</th>
+						<th class="score_after" rowspan="2">Điểm hoặc xếp loại sau KT lại (nếu có)</th>
+						<th class="teacher" rowspan="2">Giáo viên bộ môn</th>
+					</tr>
+					<tr>
+						<th class="term1">H.kỳ I</th>
+						<th class="term2">H.kỳ II</th>
+						<th class="term3">CN</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php $subjects = Subject::find('')->all();
+						foreach ($subjects as $subject) { ?>
+					<tr class="subject_row">
+						<td><?= $subject->name ?></td>
+						<td>
+						<?php
+							if($model->yearEvaluations[$i]->checkTermExist(1)){
+								$termEvaluation = $model->yearEvaluations[$i]->getTerm(1);
+								$subjectScore = $termEvaluation->getSubjectScore($subject->name);
+								$teacherName = $subjectScore->teacherName;
+							} else {
+								$subjectScore = null;
+							}
+							echo $subjectScore != null ? $subjectScore->score : null;
+						?>
+						</td>
+						<td>
+						<?php
+							if($model->yearEvaluations[$i]->checkTermExist(2)){
+								$termEvaluation = $model->yearEvaluations[$i]->getTerm(2);
+								$subjectScore = $termEvaluation->getSubjectScore($subject->name);
+								$teacherName = $subjectScore->teacherName;
+							} else {
+								$subjectScore = null;
+							}
+							echo $subjectScore != null ? $subjectScore->score : null;
+						?>	
+						</td>
+						<td>
+						<?php
+							if($model->yearEvaluations[$i]->checkTermExist(0)){
+								$termEvaluation = $model->yearEvaluations[$i]->getTerm(0);
+								$subjectScore = $termEvaluation->getSubjectScore($subject->name);
+								$teacherName = $subjectScore->teacherName;
+							} else {
+								$subjectScore = null;
+							}
+							echo $subjectScore != null ? $subjectScore->score : null;
+						?>	
+						</td>
+						<td></td>
+						<td><?= isset($teacherName) ? $teacherName : null ?></td>
+					</tr>
+					<?php } ?>
+					<tr class"average">
+						<td>ĐTB các môn</td>
+						<td>
+						<?php
+							if($model->yearEvaluations[$i]->checkTermExist(1)){
+								$termEvaluation = $model->yearEvaluations[$i]->getTerm(1);
+							} else {
+								$termEvaluation = null;
+							}
+							echo $termEvaluation != null ? $termEvaluation->getAverageScore() : null;
+						?>	
+						</td>
+						<td>
+						<?php
+							if($model->yearEvaluations[$i]->checkTermExist(2)){
+								$termEvaluation = $model->yearEvaluations[$i]->getTerm(2);
+							} else {
+								$termEvaluation = null;
+							}
+							echo $termEvaluation != null ? $termEvaluation->getAverageScore() : null;
+						?>
+						</td>
+						<td>
+						<?php
+							if($model->yearEvaluations[$i]->checkTermExist(0)){
+								$termEvaluation = $model->yearEvaluations[$i]->getTerm(0);
+							} else {
+								$termEvaluation = null;
+							}
+							echo $termEvaluation != null ? $termEvaluation->getAverageScore() : null;
+						?>
+						</td>
+						<td></td>
+						<td><?= $model->yearEvaluations[$i]->teacherName ?></td>
+					</tr>
+					<tr class="comment">
+						<td colspan="6"><?= $model->yearEvaluations[$i]->note ?>
+						</td>
+					</tr>
+					<tr class="confirm">
+						<td class="teacher_confirm" colspan="3">
+							<div class="text1" style="font-weight: bold;">Xác nhận của giáo viên chủ nhiệm</div>
+							<div class="text2">(Ký và ghi rõ họ tên)</div>
+							<div class="name" style="font-weight: bold;"><?= $model->yearEvaluations[$i]->teacherName ?></div>
+						</td>
+						<td class="principal_confirm" colspan="3">
+							<div class="text1" style="font-weight: bold;">Xác nhận của Hiệu trưởng</div>
+							<div class="text2" style="font-weight: normal;">(Ký, ghi rõ họ tên và đóng dấu)</div>
+							<div class="name" style="font-weight: bold;"><?= $model->yearEvaluations[$i]->principalName ?></div>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<div id="part2"></div>
+	</div>
+
+	<pagebreak />
+
+	<div id="conduct_capacity">
+		<div class="school"></div>
+		<table>
+			<tr class="bold">
+				<td rowspan="2">HỌC KỲ</td>
+				<td colspan="2">Kết quả xếp loại</td>
+				<td rowspan="2">TS buổi nghỉ học cả năm</td>
+				<td colspan="2" class="order_after">Xếp loại sau KT lại các môn học hoặc rèn luyện thêm về HK</td>
+				<td class="upgrade" rowspan="5"><?= $model->yearEvaluations[$i]->upGradeType ?></td>
+			</tr>
+			<tr class="bold">
+				<td>HK</td>
+				<td>HL</td>
+				<td>HK</td>
+				<td>HL</td>
+			</tr>
+			<tr>
+				<td class="term1">Học kỳ I</td>
+				<td><?= $model->yearEvaluations[$i]->getConduct(1); ?></td>
+				<td><?= $model->yearEvaluations[$i]->getLearnCapacity(1); ?></td>
+				<td rowspan="3" class="miss_content"><?= $model->yearEvaluations[$i]->missedLesson ?></td>
+				<td rowspan="3"></td>
+				<td rowspan="3"></td>
+			</tr>
+			<tr>
+				<td>Học kỳ II</td>
+				<td><?= $model->yearEvaluations[$i]->getConduct(2); ?></td>
+				<td><?= $model->yearEvaluations[$i]->getLearnCapacity(2); ?></td>
+			</tr>
+			<tr>
+				<td>Cả năm</td>
+				<td><?= $model->yearEvaluations[$i]->getConduct(0); ?></td>
+				<td><?= $model->yearEvaluations[$i]->getLearnCapacity(0); ?></td>
+			</tr>
+			<tr id="certificate">
+				<td class="vocational_certificate" colspan="6">- Có chứng chỉ nghề phổ thông: <?= $model->yearEvaluations[$i]->vocationalCertificate ?>
+				</td>
+				<td colspan="1">Loại: <?= $model->yearEvaluations[$i]->vocationalCertificateLevel ?></td>
+			</tr>
+			<tr id="achievement">
+				<td colspan="7">
+				- Các giải thưởng và khen thưởng: <?= $model->yearEvaluations[$i]->getAchievementString() ?>
+				</td>
+			</tr>
+			<tr id="teacher_comment">
+				<td colspan="7" class="title1">NHẬN XÉT CỦA GIÁO VIÊN CHỦ NHIỆM</td>
+			</tr>
+			<tr id="teacher_comment" class="title2">
+				<td colspan="7">(Ký và ghi rõ họ tên)</td>
+			</tr>
+			<tr id="teacher_comment" class="content">
+				<td colspan="7"><?= $model->yearEvaluations[$i]->teacherComment ?></td>
+			</tr>		
+			<tr id="teacher_comment" class="teacher_name">
+				<td colspan="7"><?= $model->yearEvaluations[$i]->teacherName ?></td>
+			</tr>	
+
+			<tr id="principal_approval" class="title1">
+				<td colspan="7">PHÊ DUYỆT CỦA HIỆU TRƯỞNG</td>
+			</tr>
+			<tr id="principal_approval" class="content">
+				<td colspan="7"><?= $model->yearEvaluations[$i]->principalApproval ?></td>
+			</tr>
+			<tr id="principal_approval" class="date">
+				<td colspan="7">
+				<?php $timestamp = strtotime($model->yearEvaluations[$i]->date);
+						echo $model->studyProcesses[$i]->school->address->district->name.', ngày '.date('d', $timestamp).' tháng '.date('m', $timestamp).' năm '.date('y', $timestamp); ?>
+				</td>
+			</tr>
+			<tr id="principal_approval" class="title2">
+				<td colspan="7">Hiệu trưởng</td>
+			</tr>
+			<tr id="principal_approval" class="title3">
+				<td colspan="7">(Phê duyệt, ký, ghi rõ họ tên và đóng dấu)</td>
+			</tr>
+			<tr id="principal_approval" class="principal_name">
+				<td colspan="7"><?= $model->yearEvaluations[$i]->principalName ?></td>
+			</tr>
+			
+		</table>
+	</div>	
+	<?php }
+	} ?>
 </div>
