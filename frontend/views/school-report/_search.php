63a440eaeb0af8424cf79e99d3c02107a37af91a
hung3102 <daovanhung3107@yahoo.com>
2016-05-03 14:21:28 +0700

    add frontend/view/school-report

---
+++ frontend/views/school-report/_search.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\search\SchoolReportSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="school-report-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'number') ?>
+
+    <?= $form->field($model, 'studentID') ?>
+
+    <?= $form->field($model, 'date') ?>
+
+    <?= $form->field($model, 'principalName') ?>
+
+    <?php // echo $form->field($model, 'created_time') ?>
+
+    <?php // echo $form->field($model, 'updated_time') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
