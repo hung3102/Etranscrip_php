fe4d3965139734a2c3d415e1cb40aa64c62ede45
hung3102 <daovanhung3107@yahoo.com>
2016-05-12 15:32:58 +0700

    subject complete

--- backend/views/subject/_form.php
+++ backend/views/subject/_form.php
@@ -3,9 +3,6 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
-/* @var $this yii\web\View */
-/* @var $model common\models\Subject */
-/* @var $form yii\widgets\ActiveForm */
 ?>
 
 <div class="subject-form">
@@ -13,10 +10,6 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'created_time')->textInput() ?>
-
-    <?= $form->field($model, 'updated_time')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
